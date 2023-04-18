015bfc612be74366a34d08bb302f0aa6b2f3e968
Sandor Miglecz <miglecz.sandor@gmail.com>
2023-04-18 19:46:00 +0000

    Upload New File

---
+++ ModelFactory.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: smiglecz
+ * Date: 5/4/18
+ * Time: 3:34 PM
+ */
+
+include_once "User.php";
+include_once "Team.php";
+include_once "WorkloadScheme.php";
+include_once "HolidayScheme.php";
+
+class ModelFactory {
+
+    public static function make($className, $object) {
+        if (!is_object($object)) {
+            throw new Exception("Not object to instantiate");
+        }
+        if (get_class($object) == $className) {
+            return $object;
+        }
+        $idField = $className::$idField;
+        $id = $object->$idField;
+        $instance = $className::getById($id);
+        if (!$instance) {
+            $instance = new $className($object);
+        }
+        return $instance;
+    }
+
+    public static function makeMultiple($className, $objects) {
+        $instances = array();
+        foreach ($objects as $object) {
+            $instances[] = static::make($className, $object);
+        }
+        return $instances;
+    }
+}
